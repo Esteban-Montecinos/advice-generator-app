09258555728c437bddc46cfeaa6b29d2412c481f
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-07-31 00:24:27 -0400

    style: drop-shadow

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import HomeClient from "./client";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center drop-shadow-[5px_15px_35px_rgba(0,0,0,0.15)]">
       <HomeClient/>
     </main>
   );
