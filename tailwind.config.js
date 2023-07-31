b3f48d388099ebe19e7c8301fb303981f33e19b9
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-07-31 00:24:01 -0400

    style: drop-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,10 @@ module.exports = {
   theme: {
     extend: {
       dropShadow: {
-        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
-        '4xl': [
-            '0 35px 35px rgba(0, 0, 0, 0.25)',
-            '0 45px 65px rgba(0, 0, 0, 0.15)'
-        ]
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)'
+      },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
       },
     },
   },
